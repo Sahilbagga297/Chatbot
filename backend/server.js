85fd4169e0c28c90e5ca65e67f2af081d104f8a5
Sahil Bagga <sahilbagga297@gmail.com>
2026-02-06 16:05:20 +0530

    frontend optimized and cors config corrected

--- backend/server.js
+++ backend/server.js
@@ -31,7 +31,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+const corsOptions = {
+    origin: process.env.FRONTEND_BASE_URL || "*",
+    credentials: true
+};
+app.use(cors(corsOptions));
 app.use(express.json());
 
 // Routes
